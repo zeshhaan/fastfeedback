28e079b0afe6b0672d1b0f70ed4e504b4f71a876
Mohammed Zeeshan <34814964+zeshhaan@users.noreply.github.com>
2021-01-23 21:57:05 +0530

    Added authentication using firebase (#2)
    Co-authored-by: Mohammed Zeeshan <34814964+zeeshanofficial@users.noreply.github.com>

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,9 @@
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
+import { useAuth } from "../lib/auth";
 
-export default function Home() {
+const Home = () => {
+  const auth = useAuth();
   return (
     <div className={styles.container}>
       <Head>
@@ -11,41 +13,18 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>Fast Feedback</h1>
-
         <p className={styles.description}>
           Get started by editing{" "}
           <code className={styles.code}>pages/index.js</code>
         </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+        {auth.user ? (
+          <div className={styles.grid}>
+            <p>Email: {auth.user.email}</p>
+            <button onClick={(e) => auth.signout()}>Sign Out</button>
+          </div>
+        ) : (
+          <button onClick={(e) => auth.signinWithGitHub()}>Sign In</button>
+        )}
       </main>
 
       <footer className={styles.footer}>
@@ -60,4 +39,6 @@ export default function Home() {
       </footer>
     </div>
   );
-}
+};
+
+export default Home;
